0d2a52c5211e3d8eb537e84c09c97063f789fca3
MD Shahin <esjgroupinc7@gmail.com>
2022-10-10 22:59:35 +0600

    Navbar Create

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Header() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home"><span className='text-warning fs-3 fw-bold'>Quick Quiz</span></Navbar.Brand>
+          <Nav className="justify-content-end">
+            <Nav.Link href="#home"><span className='text-white'>Topic</span></Nav.Link>
+            <Nav.Link href="#features"><span className='text-white'>Statistic</span></Nav.Link>
+            <Nav.Link href="#pricing"><span className='text-white'>Blog</span></Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default Header;
